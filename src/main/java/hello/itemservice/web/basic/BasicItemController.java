d013b330aade73fa9e593e68a49332a457c43a8e
oeunkyoung <ou1112@naver.com>
2022-05-12 23:11:28 +0900

    상품 등록 폼

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -26,6 +27,23 @@ public class BasicItemController {
         return "/basic/items";
     }
 
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "/basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "xxx";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
@@ -33,13 +51,6 @@ public class BasicItemController {
     public void init() {
         itemRepository.save(new Item("ItemA", 10000, 10));
         itemRepository.save(new Item("ItemA", 20000, 20));
-    }
-
-    @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId, Model model) {
-        Item item = itemRepository.findById(itemId);
-        model.addAttribute("item", item);
-        return "basic/item";
     }
 
 }
